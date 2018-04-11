38891698a494e004db35c83b7351cacd37baa84d
peaceChierdo <449780013@qq.com>
2018-04-11 17:48:52 +0800

    add pic for REAEME

---
+++ 函数防抖/router.js
@@ -0,0 +1,11 @@
+router.get('/loadMore', function(req, res) {
+	var nextIndex = req.query.index,
+		len = req.query.length,
+		data = []
+	for(var i=0; i< len; i++){
+		data.push('内容'+(parseInt(nextIndex) + i))
+	}
+	setTimeout(function(){
+		res.send(data)
+	}, 3000)
+})
